f499e57962f91cc3baac4c128c58f0de6f257789
rlawnahd <rlawnahd123@naver.com>
2023-10-17 13:45:49 +0900

    deploy:배포준비

---
+++ src/middleware.ts
@@ -0,0 +1,32 @@
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
+
+export { default } from 'next-auth/middleware';
+
+export async function middleware(req: NextRequest) {
+    const token = await getToken({ req });
+    if (!token) {
+        if (req.nextUrl.pathname.startsWith('/api')) {
+            return new NextResponse('Unauthorized', { status: 401 });
+        }
+
+        const { pathname, search, origin, basePath } = req.nextUrl;
+        const signInUrl = new URL(`${basePath}/auth/signin`, origin);
+        signInUrl.searchParams.append('callbackUrl', `${basePath}${pathname}${search}`);
+        return NextResponse.redirect(signInUrl);
+    }
+    return NextResponse.next();
+}
+
+export const config = {
+    matcher: [
+        '/new',
+        '/',
+        '/api/bookmakrs',
+        '/api/comments',
+        '/api/likes',
+        '/api/follow',
+        '/api/me',
+        '/api/posts/:path*',
+    ],
+};
